b01b170fe052e84e87c9ffdae92e37b64e57bebb
Martin Scasny <martin.scasny23@gmail.com>
2022-11-22 22:14:36 +0100

    ngram init

---
+++ collectFeatures.py
@@ -0,0 +1,9 @@
+import feature_extractor
+
+def collectNGrams(values,result):
+    # values ngramy z aktualneho suboru, musia byt unikatne, tie prida a pripocita count, cize pole poli
+    # nakoniec vyberie n najcastejsich
+    
+    
+def selectMostCommon(result,n):
+    pass
