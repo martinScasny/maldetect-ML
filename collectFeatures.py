8679a12905075d61c6bbaa827a80700ffa2f2000
Martin Scasny <martin.scasny23@gmail.com>
2022-12-04 12:19:15 +0100

    collect features, ctype integration with nsort (nsort segfault)

--- collectFeatures.py
+++ collectFeatures.py
@@ -1,6 +1,6 @@
 import feature_extractor
 import os
-import ctypes
+from ctypes import *
 import numpy as np
 
 def collectNGrams(filepath,ngramSize,ngramCount):
@@ -9,8 +9,14 @@ def collectNGrams(filepath,ngramSize,ngramCount):
     # [ins][count]
     # sort by count
     # 
-    py_values = list()
-    arr = (ctypes.c_ushort * (pow(2,32)))(*py_values)
+    path = r'C:\Users\Martin\Desktop\Anti-malware-tool\Anti-malware-tool\nsort.so'
+    libObject = CDLL(path)
+    nsort = libObject.findNTopValues
+    nsort.argtypes = [c_int, POINTER(c_uint16), c_uint16]
+    nsort.restype = POINTER(POINTER(c_uint16))
+    arr = (c_uint16 * 4294967296)()
+    arrPtr = pointer(arr)
+    ntopValues = POINTER(POINTER(c_uint16))
     
     mypath = filepath
     # collect all ngram values 
@@ -18,31 +24,27 @@ def collectNGrams(filepath,ngramSize,ngramCount):
         print('Collecting values from file:',file)
         values = feature_extractor.getNgram(f"{filepath}\\{file}",ngramSize)
         for x in values:
-            arr[x] += 1
+            arrPtr.contents[x] = c_uint16(arrPtr.contents[x]+1)
+
         print('Values were collected...')
     print("Sorting is happening ...")
     # replace with C func
-    ntopValues = nsort(arr)
-    sorted_arr = np_arr[np.argsort(np_arr[:,0],kind='heapsort')]
+    ntopValues = nsort(c_int(ngramCount),arrPtr.contents,4294967295)
     # 
     print("DONE :)")
-    result = sorted_arr.tolist()[-ngramCount:]
-    return [result[x][1] for x in range(len(result))]
+    result = []
+    for x in range(ngramCount):
+        result.append(ntopValues.content[x])
+    
+    return result
     
     
-# arr = [[0,x] for x in range(10)]
-# result = np.array(arr)
-# print(arr)
-# values = [np.random.randint(10) for x in range(10)]
-# print(values)
-# for x in values:
-#     result[x][0] += 1
-# print(result)
-# sorted_index = result[np.argsort(result[:, 0],kind='heapsort')]
 
-# # print(reversed(list(sorted_index[-3:]))[:,1])
-# result = sorted_index.tolist()[-3:]
-# print([result[x][1] for x in range(len(result))])
-
-topNGrams = collectNGrams('C:\\Users\\Martin\\Desktop\\Samples',3,2000)
+topNGrams = collectNGrams('C:\\Users\\Martin\\Desktop\\Samples',4,2000)
 print(topNGrams)
+
+# arr = (c_uint16 * (pow(255,4)))()
+# arrPtr = pointer(arr)
+# arrPtr.contents[0] = 23
+# arrPtr.contents[1] = 54
+# print(arrPtr.contents[0],arrPtr.contents[1])
